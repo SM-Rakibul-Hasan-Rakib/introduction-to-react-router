1738a22b8b1a7d1f462627d9b031da24efb93fa0
SM-Rakibul-Hasan-Rakib <devrakibulhasanrakib@gmail.com>
2024-03-21 12:28:04 +0600

    100% done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Header.css";
 const Header = () => {
   return (
@@ -6,11 +6,16 @@ const Header = () => {
       <h2>Navbar</h2>
       <nav>
         <span>My website</span>
-        <Link to="/">Home</Link>
-        <Link to="/users">Users</Link>
-        <Link to="/posts">posts</Link>
-        <Link to="/about">About</Link>
-        <Link to="/Contact">Contact Us</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/users">Users</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/posts">Posts</NavLink>
+        <NavLink to="/contact">Contact Us</NavLink>
+          {/* <Link to="/">Home</Link> */}
+        {/* <Link to="/users">Users</Link> */}
+        {/* <Link to="/posts">posts</Link> */}
+        {/* <Link to="/about">About</Link> */}
+        {/* <Link to="/Contact">Contact Us</Link> */}
         {/* <h2>name komu shorom kora</h2> */}
         {/* <a href="/">Home</a> */}
         {/* <a href="/about">About</a> */}
